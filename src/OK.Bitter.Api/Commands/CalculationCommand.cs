d50b0af0bdccec986517a7c4059ced3f40bc0381
Oğuzhan Kiyar <oguzhan.kiyar@outlook.com>
2021-04-05 00:39:54 +0300

    Add calculation command balance impl

--- src/OK.Bitter.Api/Commands/CalculationCommand.cs
+++ src/OK.Bitter.Api/Commands/CalculationCommand.cs
@@ -13,17 +13,57 @@ namespace OK.Bitter.Api.Commands
     {
         private readonly ITradeRepository _tradeRepository;
         private readonly ISymbolRepository _symbolRepository;
+        private readonly UserBalanceCalculation _userBalanceCalculation;
         private readonly UserPnLCalculation _userPnLCalculation;
 
         public CalculationCommand(
             ITradeRepository tradeRepository,
             ISymbolRepository symbolRepository,
+            UserBalanceCalculation userBalanceCalculation,
             UserPnLCalculation userPnLCalculation,
             IServiceProvider serviceProvider) : base(serviceProvider)
         {
             _tradeRepository = tradeRepository ?? throw new System.ArgumentNullException(nameof(tradeRepository));
             _symbolRepository = symbolRepository ?? throw new System.ArgumentNullException(nameof(symbolRepository));
+            _userBalanceCalculation = userBalanceCalculation ?? throw new System.ArgumentNullException(nameof(userBalanceCalculation)); ;
             _userPnLCalculation = userPnLCalculation ?? throw new System.ArgumentNullException(nameof(userPnLCalculation)); ;
+        }
+
+        [CommandCase("balance", "{symbol}")]
+        public async Task BalanceAsync(string symbol)
+        {
+            var trades = _tradeRepository.GetList(x => x.UserId == User.Id);
+
+            if (symbol != "all")
+            {
+                var symbolEntity = _symbolRepository.Get(x => x.Name == symbol.ToUpperInvariant() || x.FriendlyName == symbol.ToUpperInvariant());
+                if (symbolEntity == null)
+                {
+                    await ReplyAsync("Symbol is not found!");
+
+                    return;
+                }
+
+                trades = trades.Where(x => x.SymbolId == symbolEntity.Id);
+            }
+
+            if (!trades.Any())
+            {
+                await ReplyAsync("There are no trades!");
+
+                return;
+            }
+
+            await _userBalanceCalculation.CalculateAsync(trades.Select(x => new TradeModel
+            {
+                Ticket = x.Ticket,
+                UserId = x.UserId,
+                SymbolId = x.SymbolId,
+                Type = x.Type,
+                Volume = x.Volume,
+                Price = x.Price,
+                Time = x.Time
+            }).ToList());
         }
 
         [CommandCase("pnl", "{symbol}")]
